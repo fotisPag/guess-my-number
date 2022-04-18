8412f3a052e3cf1f7471d300e5ef59d8db9cda8b
fotisPag <fotis.pagonis01@gmail.com>
2022-04-18 10:16:31 +0300

    refactored code

--- script.js
+++ script.js
@@ -1,18 +1,5 @@
 'use strict';
 
-/*console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.message').textContent = 'Correct Number!';
-
-console.log(document.querySelector('.message').textContent);
-
-document.querySelector('.number').textContent = 13;
-document.querySelector('.score').textContent = 10;
-
-document.querySelector('.guess').value = 23;
-console.log(document.querySelector('.guess').value);
-*/
-
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
@@ -34,22 +21,10 @@ document.querySelector('.check').addEventListener('click', function () {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-  }
-  //if the number is bigger than the actual
-  else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = 'Too high';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game';
-      document.querySelector('.score').textContent = 0;
-    }
-  }
-  //if the number is smaller
-  else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too low';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high' : 'Too low';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
